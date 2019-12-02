e4a1023ecf6937efacc2198278b516305701dc32
Airtonzentokimura <airtonzento@gmail.com>
2019-12-02 19:32:27 -0300

    Aula 19 Destruindo o tiro pela distância do disparo concluida

--- Rogue tank/Scripts/Bullet.gd
+++ Rogue tank/Scripts/Bullet.gd
@@ -1,18 +1,27 @@
 extends Area2D
 
+#numero que nunca muda de distancia.
+const MAX_DIST = 250
+
 var vel = 250
 #setget e utilizado para pegar as defincoes de uma funcao
 var dir = Vector2(0,-1) setget set_dir
+#variavel que marca posicao
+onready var init_pos = global_position
 
 func _ready():
+	
 	pass
 	
 func _process(delta):
+	# se a minha posicao (global position) ate a distancia (distance to a b) foi maior que 250
+	if global_position.distance_to(init_pos) >= MAX_DIST:
+	#autodestroi
+		queue_free()
+	
 	translate(dir*vel*delta)
 	
 	
-
-
 #função da saida de objeto
 func _on_VisibilityNotifier2D_screen_exited():
 	#funcao que dele quando sai da tela
