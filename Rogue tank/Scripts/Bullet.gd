d41cfbccd37e507dbc5377a14a48c48975d7e4ec
Airton Zento Kimura <53239311+Airtonzentokimura@users.noreply.github.com>
2019-12-05 21:39:11 -0300

    Aula 20 - Efeito de autodestruição do projétil.

--- Rogue tank/Scripts/Bullet.gd
+++ Rogue tank/Scripts/Bullet.gd
@@ -6,6 +6,8 @@ const MAX_DIST = 250
 var vel = 250
 #setget e utilizado para pegar as defincoes de uma funcao
 var dir = Vector2(0,-1) setget set_dir
+#variável para vivo ligada como verdadeiro
+var live = true
 #variavel que marca posicao
 onready var init_pos = global_position
 
@@ -14,12 +16,28 @@ func _ready():
 	pass
 	
 func _process(delta):
-	# se a minha posicao (global position) ate a distancia (distance to a b) foi maior que 250
-	if global_position.distance_to(init_pos) >= MAX_DIST:
-	#autodestroi
-		queue_free()
 	
-	translate(dir*vel*delta)
+	if live:
+	# se a minha posicao (global position) ate a distancia (distance to a b) foi maior que 250
+		if global_position.distance_to(init_pos) >= MAX_DIST:
+		
+		#$ = patio ou nó smoke .emitting= continuar animação
+			$Smoke.emitting = false
+		#vivo sera falso e nao ira se movimentar
+			live = false
+		#$ = patio ou no Sprite .visible = visibilidade (desenho nao sera visivel)
+			$Sprite.visible = false
+		#monitoring = colisão false = desligado monitorable = colisão
+			monitoring = false
+			monitorable = false
+		#$ = patio ou no Anim_self_destruction .play = faz a animação "explode" nome da animação
+			$Anim_self_destruction.play("explode")
+		# yield = de preferencia a e espere a execucao (argumento 1 que e o patio ou no , argumento 2 sinal do no ou patio)
+			yield($Anim_self_destruction , "animation_finished")
+		#autodestroi
+			queue_free()
+		
+		translate(dir*vel*delta)
 	
 	
 #função da saida de objeto
